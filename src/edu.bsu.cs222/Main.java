b8a49cd19de3aa65b2356a248fb6598cca5172c7
Jacob Rosner <jwrosner@bsu.edu>
2019-10-12 15:36:53 -0400

    url connection test

--- src/edu.bsu.cs222/Main.java
+++ src/edu.bsu.cs222/Main.java
@@ -1,6 +1,11 @@
+import java.net.URL;
+import java.net.URLConnection;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+
+
     }
+
 }
--- test/edu.bsu.cs222/test.java
+++ test/edu.bsu.cs222/test.java
@@ -1,3 +1,24 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.net.URL;
+import java.net.URLConnection;
+
 public class test {
+    @Test
+    public void  connectToGoogle() {
+        URLConnection urlConnection = null;
+        boolean connected=false;
+        try {
+            URL url = new URL(" https://newsapi.org/v1/articles?source=bbc-news&sortBy=top&apiKey=36033f4c106f44bd955f13e926095fad");
+            urlConnection = url.openConnection();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        if (urlConnection != null){
+            connected=true;
+        }
+            Assert.assertEquals(connected,true);
+    }
 
 }
