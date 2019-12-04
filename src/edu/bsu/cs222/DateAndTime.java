d4ae3484b7d3138d6f397d2c557cd46fd4b73a9f
jacobr0411 <53231773+jacobr0411@users.noreply.github.com>
2019-12-04 12:44:52 -0500

    Merge branch 'master' of C:\Users\jrosn\Documents\CS222\The-Daily-Bit-1 with conflicts.

---
+++ src/edu/bsu/cs222/DateAndTime.java
@@ -0,0 +1,10 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class DateAndTime {
+     String getDateAndTime(){
+        LocalDateTime dateTime = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM dd yyyy HH:mm");
+        return dateTime.format(formatter);
+    }
+}
---
+++ test/edu/bsu/cs222/DateAndTimeTest.java
@@ -0,0 +1,14 @@
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DateAndTimeTest {
+
+    @Test
+    void getDateAndTime() {
+        DateAndTime dateAndTime = new DateAndTime();
+        String date = dateAndTime.getDateAndTime();
+        System.out.println(date);
+    }
+}
