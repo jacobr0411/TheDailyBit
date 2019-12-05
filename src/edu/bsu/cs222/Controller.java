84b618ffe3c077c48d809f3d37e72d774240ce9c
Jacob Rosner <jwrosner@bsu.edu>
2019-12-04 19:08:27 -0500

    changed gui layout

--- src/edu/bsu/cs222/Controller.java
+++ src/edu/bsu/cs222/Controller.java
@@ -3,12 +3,17 @@ package edu.bsu.cs222;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 
 import java.io.InputStream;
 import java.util.Scanner;
 
 public class Controller {
 
+    public TextField searchTerm;
+    InputStream stream = null;
+    JSONParser parser = new JSONParser();
+    SourceSearch sourceSearch = new SourceSearch();
     public ListView<String> listView;
     public Button button;
 
@@ -16,14 +21,15 @@ public class Controller {
         System.out.println("Is this thing working?");
     }
 
-    public void titleCliked(){
-
+    public void titleCliked() throws Exception {
+        int input = listView.getSelectionModel().getSelectedIndex();
+        parser.getURLContent(input);
     }
 
     public void initialize(){
-        InputStream stream = null;
-        JSONParser parser = new JSONParser();
-        SourceSearch sourceSearch = new SourceSearch();
+//        InputStream stream = null;
+//        JSONParser parser = new JSONParser();
+//        SourceSearch sourceSearch = new SourceSearch();
 
         System.out.println("Welcome to the Daily Bit.\n\nHere are the top headlines for the day:");
 
